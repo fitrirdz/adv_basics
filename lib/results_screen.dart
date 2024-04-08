a923c8703c453cf644b6821ca9ec1c087feab3a9
Fitri R Dewi <fitri@softwareseni.com>
2024-04-08 12:00:01 +0700

    Add questions summary screen

--- lib/results_screen.dart
+++ lib/results_screen.dart
@@ -1,9 +1,28 @@
+import 'package:adv_basics/data/questions.dart';
+import 'package:adv_basics/questions_summary.dart';
 import 'package:flutter/material.dart';
 
 class ResultsScreen extends StatelessWidget {
   const ResultsScreen({super.key, required this.chosenAnswer});
 
   final List<String> chosenAnswer;
+
+  List<Map<String, Object>> getSummaryData() {
+    final List<Map<String, Object>> summary = [];
+
+    for (var i = 0; i < chosenAnswer.length; i++) {
+      summary.add(
+        {
+          'question_index': i,
+          'question': questions[i].text,
+          'correct_answer': questions[0].answers,
+          'user_answer': chosenAnswer[i],
+        },
+      );
+    }
+
+    return summary;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -18,7 +37,7 @@ class ResultsScreen extends StatelessWidget {
             const SizedBox(
               height: 30,
             ),
-            const Text('List of answers and questions . . .'),
+            QuestionsSummary(getSummaryData()),
             const SizedBox(
               height: 30,
             ),
